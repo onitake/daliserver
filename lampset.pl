80a422140b760ba5aa83e4d5a34fbf4ca31cb821
Gregor Riepl <onitake@gmail.com>
2011-04-30 23:02:58 +0200

    Added better command line parsing to lampset script

--- lampset.pl
+++ lampset.pl
@@ -5,8 +5,27 @@ use warnings;
 use usbdali;
 use Data::Dumper;
 
-my $lamp = $ARGV[0] || 0;
+if (@ARGV < 2) {
+	print("Usage: lampset.pl <0-15> <0-254>\n");
+	exit(1);
+}
+
+my $lamp = $ARGV[0];
+if ($lamp < 0) {
+	$lamp = 0;
+}
+if ($lamp > 15) {
+	$lamp = 15;
+}
+my $dim = $ARGV[1];
+if ($dim < 0) {
+	$dim = 0;
+}
+if ($dim > 254) {
+	$dim = 254;
+}
+
 my $dali = usbdali->new('localhost');
 $dali->connect() || die;
-$dali->send($dali->make_dim('lamp', $lamp, 80));
+$dali->send($dali->make_dim('lamp', $lamp, $level));
 $dali->disconnect();
