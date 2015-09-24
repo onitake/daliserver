f18a969508800dbd0bd8d8ae31bc66a7e9e6652b
Gregor Riepl <onitake@gmail.com>
2015-09-24 15:12:07 +0200

    Added connection test script

---
+++ perl/test.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use usbdali;
+use Data::Dumper;
+
+my $host = $ARGV[0] || '127.0.0.1';
+
+my $dali = usbdali->new($host);
+if ($dali->connect()) {
+	$dali->send(usbdali->make_cmd('broadcast', 'off'));
+	my $resp = $dali->receive();
+	if ($resp) {
+		print("Received status:$resp->{status} response:$resp->{response}\n");
+	} else {
+		print("Receive error\n");
+	}
+	$dali->disconnect();
+} else {
+	print("Can't connect\n");
+}
